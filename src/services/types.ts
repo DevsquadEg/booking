27ad2a699280c2a3f7a2ddce5bf2a12cca3f80d4
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-07-05 01:58:15 +0300

    declare some types

--- src/services/types.ts
+++ src/services/types.ts
@@ -7,3 +7,33 @@ export type validationType = {
     newPassword: string
   ) => object;
 };
+
+export type DecodedTokenPayload = {
+  exp: number;
+  iat: number;
+  role: string;
+  verified: boolean;
+  _id: string;
+};
+
+export type FullUserDataType = {
+  country: string;
+  createdAt: Date | string;
+  email: string;
+  phoneNumber: number;
+  profileImage: string;
+  role: "admin" | "portal";
+  updatedAt: Date | string;
+  userName: string;
+  verified: boolean;
+  _id: string;
+};
+
+export type AuthContextType = {
+  isLoading: boolean;
+  loginData: DecodedTokenPayload | null;
+  fullUserData: FullUserDataType | null;
+  logOutUser: () => void;
+  saveLoginData: () => void;
+  getCurrentUser: () => void;
+};
