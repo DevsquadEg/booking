0a4a510b8164717ee4870f029a8eb5a5ca8f01c6
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-07-05 02:08:44 +0300

    add auth and theme provider in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ThemeProvider } from "@mui/material";
+import theme from "./services/theme.ts";
+import AuthProvider from "./store/AuthContext/AuthContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme("light")}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
+  </StrictMode>
+);
