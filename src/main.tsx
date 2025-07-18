fd57d04f091f45e6c74534d289267e87f2b199f2
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-07-18 23:06:10 +0300

    fix merge issue

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,21 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import AuthProvider from "./store/AuthContext/AuthContext.tsx";
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { FavoriteProvider } from "./store/AuthContext/FavoriteContext.tsx";
 import { ThemeProvider } from "@mui/material";
 import theme from "./services/theme.ts";
-
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme("light")}>
       <AuthProvider>
-        <App />
+        <FavoriteProvider>
+          <App />
+        </FavoriteProvider>
       </AuthProvider>
     </ThemeProvider>
   </StrictMode>
