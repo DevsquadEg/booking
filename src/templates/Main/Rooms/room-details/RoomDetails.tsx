7df101f7826e28686df4d8118611716a9bfb0f20
onisEg <anasabdo704@gmail.com>
2025-07-18 15:00:58 +0200

    #

--- src/templates/Main/Rooms/room-details/RoomDetails.tsx
+++ src/templates/Main/Rooms/room-details/RoomDetails.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import RoomHeader from "../RoomHeader/RoomHeader";
 
 export default function RoomDetails() {
@@ -6,6 +6,7 @@ export default function RoomDetails() {
     <>
       <Container maxWidth="lg">
         <RoomHeader roomName="Village Angga" city="New York / USA" />
+
         {/* Room details content goes here */}
         <Box
           sx={{
@@ -61,6 +62,45 @@ export default function RoomDetails() {
             />
           </Box>
         </Box>
+        <Grid container spacing={4}>
+          {/* // Room description and features */}
+          <Grid>
+            <Typography variant="body2" color="text.secondary">
+              Minimal techno is a minimalist subgenre of techno music. It is
+              characterized by a stripped-down aesthetic that exploits the use
+              of repetition and understated development...
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Such trends saw the demise of the soul-infused techno that
+              typified the original Detroit sound...
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Design is a plan or specification for the construction of an
+              object or system or for the implementation...
+            </Typography>
+
+            {/* // Room features */}
+            <Grid container spacing={2} mt={2}>
+              {[
+                { icon: "🛏️", label: "5 bedroom" },
+                { icon: "🛋️", label: "1 living room" },
+                { icon: "🛁", label: "3 bathroom" },
+                { icon: "🍽️", label: "1 dining room" },
+                { icon: "📶", label: "10 mbp/s" },
+                { icon: "🔧", label: "7 unit ready" },
+                { icon: "🧊", label: "2 refrigerator" },
+                { icon: "📺", label: "4 television" },
+              ].map((item, index) => (
+                <Grid key={index}  xs={6} sm={4} md={3}>
+                  <Box display="flex" alignItems="center" gap={1}>
+                    <Typography fontSize={24}>{item.icon}</Typography>
+                    <Typography>{item.label}</Typography>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
