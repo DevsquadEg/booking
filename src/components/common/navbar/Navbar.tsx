6b0cd184c6b776f56a3060799977df79dc5bef08
onisEg <anasabdo704@gmail.com>
2025-07-19 20:29:54 +0200

    navigate logo to home

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import { Link as RouterLink } from "react-router-dom";
-import {  Link as MUILink } from "@mui/material";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Badge from "@mui/material/Badge";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { Link as MUILink } from "@mui/material";
 
-import logoMain from '../../../../public/color-logo-Ci_5FMX-.svg';
-import { useFavorite } from '@/store/AuthContext/FavoriteContext'; //   context
+import logoMain from "../../../../public/color-logo-Ci_5FMX-.svg";
+import { useFavorite } from "@/store/AuthContext/FavoriteContext"; //   context
 
 function Navbar() {
-
-  // update count on padge 
+  const navigate = useNavigate();
+  // update count on padge
   const { favoriteItemsCount, refreshFavorites } = useFavorite(); //  get count and refresh
 
- 
-
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -43,19 +42,19 @@ function Navbar() {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
@@ -65,44 +64,58 @@ function Navbar() {
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
-    >
-    </Menu>
+    ></Menu>
   );
 
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" sx={{ backgroundColor: "white", color: "#757575", boxShadow: "1" }}>
+        <AppBar
+          position="static"
+          sx={{ backgroundColor: "white", color: "#757575", boxShadow: "1" }}
+        >
           <Toolbar>
             <Typography
+              onClick={() => navigate("/")}
               variant="h6"
               noWrap
               component="div"
-              sx={{ display: { xs: 'none', sm: 'block' } }}
+              sx={{ display: { xs: "none", sm: "block" }, cursor: "pointer" }}
             >
               <img src={logoMain} alt="logoMain" />
             </Typography>
             <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-              <IconButton >
-                 <Badge badgeContent={favoriteItemsCount ?? 0} color="error"  >                  
-                 <Typography variant='button' > <MUILink underline="none" sx={{color:"#152C5B"}} component={RouterLink} to="/fav-list">  Favorites  </MUILink> </Typography>
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton>
+                <Badge badgeContent={favoriteItemsCount ?? 0} color="error">
+                  <Typography variant="button">
+                    {" "}
+                    <MUILink
+                      underline="none"
+                      sx={{ color: "#152C5B" }}
+                      component={RouterLink}
+                      to="/fav-list"
+                    >
+                      {" "}
+                      Favorites{" "}
+                    </MUILink>{" "}
+                  </Typography>
                 </Badge>
               </IconButton>
             </Box>
