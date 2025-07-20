86dffed334a889ef823ba9dbbf9ce72ddbc09e55
onisEg <anasabdo704@gmail.com>
2025-07-20 15:35:57 +0200

    handle success page

--- src/components/common/SuccessPayment/SuccessPayment.tsx
+++ src/components/common/SuccessPayment/SuccessPayment.tsx
@@ -1,19 +1,43 @@
 import { Box, Button, Grid } from "@mui/material";
-import photo from "../../../assets/imgs/sc.png"
+import photo from "../../../assets/success.svg";
 import { useNavigate } from "react-router-dom";
 export default function SuccessPayment() {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <Box component={"section"} p={3}>
-      <Grid container spacing={2} flexDirection={"column"} alignItems={"center"}>
-      <Grid size={{xs:12 , md:6}}>
-        <Box width={"100%"} component={"img"} src={photo} alt="success"/>
-      </Grid>
-      <Grid size={{xs:12 , md:6}}>
-        <Button onClick={()=>{navigate("/")}} variant="contained">Home</Button>
-      </Grid>
+    <Box component={"section"} p={3} textAlign={"center"}>
+      <Grid
+        container
+        spacing={2}
+        flexDirection={"column"}
+        alignItems={"center"}
+      >
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Box component={"img"} src={photo} alt="success" />
+        </Grid>
+        <Grid size={{ xs: 12, md: 6 }}>
+          <Button
+            onClick={() => {
+              navigate("/");
+            }}
+            variant="contained"
+            sx={{
+              borderRadius: 1,
+              textTransform: "none",
+              bgcolor: "var(--blue-color)",
+              px: 6,
+              py: 1.5,
+              mt: 3,
+              transition: "all 0.5s ease",
+              "&:hover": {
+                bgcolor: "#1a73e8",
+                boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
+              },
+            }}
+          >
+            Home
+          </Button>
+        </Grid>
       </Grid>
     </Box>
-  )
+  );
 }
-
